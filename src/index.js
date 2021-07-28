f8ba122ee2297ec3c8ce3d9e5e36807bf4315062
Ihor Nechyporenko <nechyporenko.i@gmail.com>
2021-07-28 22:02:35 +0300

    Add header, menu, sidebar & cards container

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
-import './scss/styles.scss';
+import './scss/main.scss';
+import refs from './js/refs';
+import sidebar from './sidebar.json';
+import items from './card-items.json';
+import sidebarTpl from './tamplates/sidebar.hbs';
+import cardItemsTpl from './tamplates/card-item.hbs';
 
+
+const renderSidebar = sidebarTpl(sidebar);
+refs.sidebar.insertAdjacentHTML('beforeend', renderSidebar);
+
+const renderCardList = cardItemsTpl(items);
+refs.cardList.insertAdjacentHTML('beforeend', renderCardList);
---
+++ src/js/refs.js
@@ -0,0 +1,4 @@
+export default {
+    sidebar: document.querySelector('.sidebar'),
+    cardList: document.querySelector('.cardlist__section'),
+}

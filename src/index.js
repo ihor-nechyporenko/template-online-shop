ce88a66ffa01386766f1055f93747c9e5ce87f9d
Ihor Nechyporenko <nechyporenko.i@gmail.com>
2021-07-30 13:26:26 +0300

    Add modal & logic for opening and closing

--- src/index.js
+++ src/index.js
@@ -5,9 +5,41 @@ import items from './card-items.json';
 import sidebarTpl from './tamplates/sidebar.hbs';
 import cardItemsTpl from './tamplates/card-item.hbs';
 
-
 const renderSidebar = sidebarTpl(sidebar);
 refs.sidebar.insertAdjacentHTML('afterbegin', renderSidebar);
 
 const renderCardList = cardItemsTpl(items);
 refs.cardList.insertAdjacentHTML('beforeend', renderCardList);
+
+refs.feedbackBtn.addEventListener('click', onFeedbackClick);
+refs.backdrop.addEventListener('click', onBackdropClick);
+refs.modalCloseBtn.addEventListener('click', onCloseModal);
+
+function onFeedbackClick(e) {
+  e.preventDefault();
+  onOpenModal();
+}
+
+function onOpenModal() {
+  window.addEventListener('keydown', onEscKeyPress);
+  refs.modal.classList.add('is-open');
+}
+
+function onCloseModal() {
+  window.removeEventListener('keydown', onEscKeyPress);
+  refs.modal.classList.remove('is-open');
+}
+
+function onBackdropClick(e) {
+  if (!e.target.classList.contains('modal__overlay')) {
+    return;
+  }
+
+  onCloseModal();
+}
+
+function onEscKeyPress(e) {
+  if (e.code === 'Escape') {
+    onCloseModal();
+  }
+}
--- src/js/refs.js
+++ src/js/refs.js
@@ -1,4 +1,8 @@
 export default {
-    sidebar: document.querySelector('.sidebar'),
-    cardList: document.querySelector('.cardlist__section'),
-}
+  sidebar: document.querySelector('.sidebar'),
+  cardList: document.querySelector('.cardlist__section'),
+  modal: document.querySelector('.modal'),
+  feedbackBtn: document.querySelector('.feedback__btn'),
+  backdrop: document.querySelector('.modal__overlay'),
+  modalCloseBtn: document.querySelector('.modal__button'),
+};
